128566310832e88313778454794cd94cbc055b28
FaizanS <faizanshergill@yahoo.com>
2024-07-02 23:31:23 +0500

    more tests - 3

--- lib/presentation/pages/home/widgets/modal_footer_buttons_widget.dart
+++ lib/presentation/pages/home/widgets/modal_footer_buttons_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kanban_tasks_list_flutter/presentation/button_styles.dart';
 
-
 class ModalFooterButtonsWidget extends StatelessWidget {
   final String nxtBtnName;
   final VoidCallback nextTap;
@@ -23,11 +22,13 @@ class ModalFooterButtonsWidget extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         ElevatedButton(
+            key: const Key('Key-ModalFooterButton-Cancel'),
             style: ButtonStyles.secondaryButton(context),
             onPressed: () => Navigator.of(context).pop(),
             child: Text(cancelBtnName)),
         const SizedBox(width: 20),
         ElevatedButton(
+            key: const Key('Key-ModalFooterButton-Next'),
             onPressed: nextTap,
             child: Text(nxtBtnName)),
       ],
